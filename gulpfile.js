61a0887cad5458b9b7effdf668c6637904ab85a8
Cameron Z. Smith <csmith158@butlercc.edu>
2018-03-25 03:37:56 -0500

    Updated gulpfile to use new plugin and fix tasks. Cleaned up unused plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
+'use strict';
+
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var atImport = require('postcss-import');
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
+var htmlmin = require('gulp-htmlmin');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
+gulp.task('minify', function() {
+    return gulp.src('./src/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+})
 
-gulp.task('css', function () {
-    var plugins = [
-        atImport,
-        autoprefixer,
-        cssnano
-    ];
-    return gulp.src('css/*.css')
-        .pipe(postcss())
-        .pipe(gulp.dest('css/min'));
+gulp.task('workflow', function() {
+    gulp.src('./src/css/**/*.css')
+      .pipe(sourcemaps.init())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions'],
+         cascade: false
+      }))
+      .pipe(cssnano())
+      .pipe(sourcemaps.write('./'))
+      
+    .pipe(gulp.dest('./dist/css/'))
+});
+
+gulp.task('default', function() {
+    gulp.watch('./src/**/*.html', ['minify']);
+    gulp.watch('./src/css/**/*.css', ['workflow']);
 });
